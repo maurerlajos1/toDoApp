fe48eb510770a3a4a1224a0b0091a81702f5b432
unknown <maurerlajos1@gmail.com>
2022-01-24 22:07:23 +0100

    Refactored to store items in objects + added description, delete items fixed, added saving list items to local storage

--- index.js
+++ index.js
@@ -1,26 +1,66 @@
 // document.getElementById("todoInput").addEventListener("click", myFunction);
 let i=0;
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+if(todoList.length>0)
+{
+    renderItems();
+}
 
-function myFunction() {
+function renderItems(){
+    let todoListHtml = document.getElementById("todoListContainer");
+    todoListHtml.innerHTML ='';
+
+for (let i = 0; i < todoList.length; i++) {
+    todoListHtml.innerHTML +=   '<div class="singleListItemContainer"> <div class="listItem">' + 
+    todoList[i].title + '</div> <div class="description">'+
+    todoList[i].description + '</div>' +   
+    '<div class="deleteItem" onclick="deleteItem('+i+ ')" >X</div> </div>';
     
-let todoListHtml = document.getElementById("todoListContainer")
-let inputValue = document.getElementsByClassName("todoInput")[0].value;
+}
+window.localStorage.setItem("todoList", JSON.stringify(todoList));
+}
 
-// console.log('<div class="deleteItem" onclick="deleteItem('+ i +')" >X</div>')
+function addItem() {
+ 
+let todoListItem = {
+  title: document.getElementById("todoTitle").value,
+  description: document.getElementById("toDoDescription").value
+};
 
-todoListHtml.innerHTML =  todoListHtml.innerHTML + '<div class="singleListItemContainer"> <div class="listItem">' + inputValue + '</div>' +
-'<div class="deleteItem" onclick="deleteItem('+i+ ')" >X</div> </div>';
+console.log(todoListItem);
+
+todoList.push(todoListItem)
+
+console.log(todoList);
+
+
+renderItems();
 
 
 
-document.getElementsByClassName("todoInput")[0].value = '';
+
+document.getElementById("todoTitle").value = '';
+document.getElementById("toDoDescription").value = '';
 i++;
 }
 
-function deleteItem( a ){
-    if(document.getElementsByClassName("singleListItemContainer")[a]){
-    document.getElementsByClassName("singleListItemContainer")[a].remove();
+
+
+function deleteItem( index ){
+    let reserverdArray= [];
+    for (let i = 0; i < todoList.length; i++) {
+             if(i==index){
+
+             }
+             else{
+                reserverdArray.push(todoList[i]);
+
+             }
+        
+    }
+
+    todoList  = reserverdArray;
+    renderItems();
 }
     //document.getElementsByClassName("deleteItem")[a].innerHTML = '';
     
-}
